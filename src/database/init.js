310b09e76f3f418f2683e757ff7445eb629dfb61
FelipeSiravegna <felipesiravegna1@gmail.com>
2023-01-14 16:06:19 -0300

    fix - sequelize authentication

--- src/database/init.js
+++ src/database/init.js
@@ -6,26 +6,25 @@ const { config } = require('./config');
 
 
 //create database (test/dev) if don't exists.
-if (CURRENT_ENVIROMENT === 'development' || CURRENT_ENVIROMENT === 'test') {
-  let { username, password, host } = config
-  let localPostgresUrl = `postgres://${username}:${password}@${host}/postgres`
-  let localClient = new Client(localPostgresUrl)
-
-  localClient.connect() //connect db
-
-  localClient.query(`CREATE DATABASE ${config.database}`, (error) => {
-    error
-      ? console.log(`database: ${config.database} already exists`)
-      : console.log(`database: ${config.database} created`)
-
-    localClient.end() //close postgres connection
-  })
-}
-
+let { username, password, host } = config
+let localPostgresUrl = `postgres://${username}:${password}@${host}/postgres`
+let localClient = new Client(localPostgresUrl)
 const database = new Sequelize(config)
 
-database.authenticate()
-  .then(() => console.log('Connection has been established successfully.'))
-  .catch((e) => console.error('Unable to connect to the database:', e))
+localClient.connect() //connect db
+
+localClient.query(`CREATE DATABASE ${config.database}`, (error) => {
+  error
+    ? console.log(`database: ${config.database} already exists`)
+    : console.log(`database: ${config.database} created`)
+
+  localClient.end() //close postgres connection
+
+  
+  database.authenticate()
+    .then(() => console.log('Connection has been established successfully.'))
+    .catch((e) => console.error('Unable to connect to the database:', e))
+})
+
 
 module.exports = { database }
