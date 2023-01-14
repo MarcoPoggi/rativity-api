4e1a05b3d11ec592fc16da2414c257159d3dcf89
FelipeSiravegna <felipesiravegna1@gmail.com>
2023-01-14 16:13:39 -0300

    wip - format

--- src/database/init.js
+++ src/database/init.js
@@ -1,9 +1,6 @@
-const CURRENT_ENVIROMENT = process.env.NODE_ENV
-
 const { Sequelize } = require('sequelize');
 const { Client } = require("pg")
 const { config } = require('./config');
-
 
 //create database (test/dev) if don't exists.
 let { username, password, host } = config
@@ -20,11 +17,9 @@ localClient.query(`CREATE DATABASE ${config.database}`, (error) => {
 
   localClient.end() //close postgres connection
 
-  
   database.authenticate()
     .then(() => console.log('Connection has been established successfully.'))
     .catch((e) => console.error('Unable to connect to the database:', e))
 })
-
 
 module.exports = { database }
