aecff9971495e5b8368edd42498bb500b1c2325b
Marco Poggi <marcopoggi2017@gmail.com>
2023-01-19 17:28:00 -0300

    wip

--- config/enviroments_config.js
+++ config/enviroments_config.js
@@ -1,10 +1,10 @@
 const ENV = process.env.NODE_ENV
 
 const DATABASE_DEFAULT = {
-  username: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
+  username: process.env.PGUSER || "postgres",
+  password: process.env.PGPASSWORD || "",
+  host: process.env.PGHOST || "localhost",
+  port: process.env.PGPORT || 5432,
   dialect: "postgres",
   logging: false,
   native: false
